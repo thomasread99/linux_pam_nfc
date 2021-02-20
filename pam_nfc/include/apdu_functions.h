eabbfc9a7b5c079e85b5f508b1439a28583fba9e
Thomas Read <thomasread99@gmail.com>
2021-02-20 08:58:27 -0800

    added more the adpu program

--- pam_nfc/include/apdu_functions.h
+++ pam_nfc/include/apdu_functions.h
@@ -3,6 +3,9 @@
 
 #include <nfc/nfc.h>
 
+/*Get the user ID from the mobile device*/
+char* getID();
+
 /*Transmit to the card and get the response back*/
 char* apduTransmit(nfc_device *pnd, uint8_t * capdu, size_t capdulen, uint8_t * rapdu, size_t * rapdulen);
 
--- pam_nfc/src/apdu_functions.c
+++ pam_nfc/src/apdu_functions.c
@@ -2,6 +2,67 @@
 #include <string.h>
 #include <nfc/nfc.h>
 
+#include "../include/apdu_functions.h"
+
+/*Get the user ID from the mobile device*/
+char* getID()
+{
+    /*Variable declarations*/
+    char *str;
+    nfc_device *initiator;
+    nfc_target nt;
+    nfc_context *context;
+    uint8_t capdu[264];
+    size_t capdulen;
+    uint8_t rapdu[264];
+    size_t rapdulen;
+
+    /*Start the NFC context*/
+    nfc_init(&context);
+    if (context == NULL) {
+        printf("Unable to init libnfc (malloc)\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /*Open a connection to the device*/
+    initiator = nfc_open(context, NULL);
+
+    /*Check the connection has been properly initiated*/
+    if (initiator == NULL) {
+        printf("ERROR: %s", "Umable to open NFC device.");
+        exit(EXIT_FAILURE);
+    }
+    if (nfc_initiator_init(initiator) < 0) {
+        nfc_perror(initiator, "nfc_initiator_init");
+        exit(EXIT_FAILURE);
+    }
+
+    /*Define the NFC modulation values*/
+    const nfc_modulation nmMifare = {
+        .nmt = NMT_ISO14443A,
+        .nbr = NBR_106,
+    };
+
+    /*Wait for the mobile device to be placed on the reader*/
+    printf("Waiting for device...\n");
+    while (nfc_initiator_select_passive_target(initiator, nmMifare, NULL, 0, &nt) <= 0);    
+    
+    /*AID of the application*/
+    memcpy(capdu, "\x00\xA4\x04\x00\x07\xA0\x00\x00\x02\x47\x10\x01\x00", 13);
+    capdulen = 13;
+    rapdulen = sizeof(rapdu);
+    /*Call the card transmit function*/
+    str = apduTransmit(initiator, capdu, capdulen, rapdu, &rapdulen);
+    if (str == NULL)
+        exit(EXIT_FAILURE);
+
+    /*Close the device and the context*/
+    nfc_close(initiator);
+    nfc_exit(context);
+    
+    return str;
+}
+
 /*Transmit to the card and get the response back*/
 char* apduTransmit(nfc_device *pnd, uint8_t * capdu, size_t capdulen, uint8_t * rapdu, size_t * rapdulen)
 {
