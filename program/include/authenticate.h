fe1779bb5c840c4de318559dc73f90373820fc99
Thomas Read <thomasread99@gmail.com>
2021-06-04 17:15:48 +0100

    added the licence text to each c file

--- program/include/authenticate.h
+++ program/include/authenticate.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 Thomas Read
+ *
+ * This program is free software; you
+ * can redistribute it and/or modify
+ * it as you wish. Please credit the project
+ * owner in any papers or derived work where
+ * this has been used.
+ *
+ * This project is now open source, and relies
+ * on contributions from the community for
+ * further development. Please see the README
+ * for currently required features
+ *
+ */
+
 #ifndef AUTHENTICATE
 #define AUTHENTICATE
 
